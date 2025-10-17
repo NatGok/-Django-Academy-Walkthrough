34bccafb1aea005768f6f24690267c083b336f11
Natalie Gokkaya <natkaya@yahoo.com>
2025-10-17 21:04:32 +0100

    Blog post set up with admin access

---
+++ academy/diary/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+# Create your views here.
+
+
+def landingpage(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', {'posts': posts})
